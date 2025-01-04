26d4936c37edc476170d2e4ed5bfab8693368e84
khemmapichpanyana <khemmapich@gmail.com>
2025-01-04 17:51:25 +0700

    chore: add more vars for connect DB

--- src/modules/configuration/app/configuration.ts
+++ src/modules/configuration/app/configuration.ts
@@ -4,4 +4,9 @@ export default registerAs("app", () => ({
     port: parseInt(process.env.PORT, 10) || 8000,
     genesoftApiKey: process.env.GENESOFT_API_KEY,
     databaseUrl: process.env.DATABASE_URL,
+    databaseHost: process.env.DATABASE_HOST,
+    databasePort: process.env.DATABASE_PORT,
+    databaseUser: process.env.DATABASE_USER,
+    databasePassword: process.env.DATABASE_PASSWORD,
+    databaseName: process.env.DATABASE_NAME,
 }));
