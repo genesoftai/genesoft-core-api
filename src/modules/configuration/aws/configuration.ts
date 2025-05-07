eafbcc9070576bebdfd09a967e0250485417beda
Kawin Sirikhanarat <kawinsirikhanarat@gmail.com>
2025-05-07 18:08:31 +0700

    feat: update AWS SDK dependencies and integrate CloudWatch logging
    - Added `@aws-sdk/client-cloudwatch-logs` and `@nestjs/bullmq` to dependencies.
    - Updated AWS access key environment variables to use `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`.
    - Integrated `CloudWatchLogger` into the NestJS application for enhanced logging capabilities.

--- src/modules/configuration/aws/configuration.ts
+++ src/modules/configuration/aws/configuration.ts
@@ -1,8 +1,8 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("aws", () => ({
-    awsAccessKey: process.env.AWS_ACCESS_KEY,
-    awsSecretKey: process.env.AWS_SECRET_KEY,
+    awsAccessKey: process.env.AWS_ACCESS_KEY_ID,
+    awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
     awsS3BucketName: process.env.AWS_S3_BUCKET_NAME,
     awsRegion: process.env.AWS_REGION,
     awsS3CustomerBucketName: process.env.AWS_S3_CUSTOMER_BUCKET_NAME,
